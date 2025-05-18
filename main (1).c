28d85f3ce89a92fb5f9716f8b1313fb1b2e6c503
GiihHard <gi.sz@live.com>
2025-05-18 17:55:02 -0300

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 10;
+    int numero2 = 7;
+    scanf ("%d", & numero);
+    scanf ("%d", & numero2);
+    int resultado = numero + numero2;
+    printf ("resultado é igual a %d", resultado);
+
+    return 0;
+}
